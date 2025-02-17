96327123f36f6ea0600ac47b6756301045f74797
eduar <eduar@ROCHAEL>
2025-02-17 13:20:18 -0300

    Adicionar arquivos de projeto.

---
+++ MinhasComprasApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MinhasComprasApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MinhasComprasApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MinhasComprasApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
